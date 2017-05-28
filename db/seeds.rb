98885fa08d1ae9d9e56802bb706421bd73236466
David Sires <d.sires@yahoo.com>
2017-05-28 16:36:29 +0000

    Assignemnt-9-More-CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,14 @@ posts = Post.all
     )
 end
 
+100.times do
+   Question.create!(
+       title: RandomData.random_sentence,
+       body: RandomData.random_paragraph,
+       resolve: false
+       ) 
+end
+
 puts "#{Post.count}"
 Post.find_or_create_by!(title: "Hello", body: "This is the body",)
 puts "#{Post.count}"
@@ -27,3 +35,5 @@ puts "#{Comment.count}"
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+
+puts "#{Question.count} questions created"
