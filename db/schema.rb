d28a1930ffac0206905dd27f47f1ee9a612558cf
David Sires <d.sires@yahoo.com>
2017-06-01 02:30:21 +0000

    Checkpoint-10-Topics

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522002924) do
+ActiveRecord::Schema.define(version: 20170601013906) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -41,7 +41,10 @@ ActiveRecord::Schema.define(version: 20170522002924) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "topic_id"
   end
+
+  add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
@@ -49,6 +52,14 @@ ActiveRecord::Schema.define(version: 20170522002924) do
     t.boolean  "resolve"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
