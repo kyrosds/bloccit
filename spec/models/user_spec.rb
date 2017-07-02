4c3aceeb7a903c4ab81ccd93b3e52d22895fe373
David Sires <d.sires@yahoo.com>
2017-07-02 18:10:28 +0000

    Assignment-12-Authentication-User-model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+  
    # Shoulda tests for name
    it { is_expected.to validate_presence_of(:name) }
    it { is_expected.to validate_length_of(:name).is_at_least(1) }
@@ -20,6 +21,12 @@ RSpec.describe User, type: :model do
    describe "attributes" do
      it "should have name and email attributes" do
        expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
+     end
+     
+     it "should format user's name" do 
+         user.name = "steve jobs"
+         user.save
+         expect(user.name).to eq("Steve Jobs")
      end
    end
   
